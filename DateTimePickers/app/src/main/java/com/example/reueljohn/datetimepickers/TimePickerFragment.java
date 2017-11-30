b2efc659dbde82c0e4583afee4354419b6f41531
Reuel John <reueljohn@MAC077.local>
2017-11-30 13:44:50 +0800

    4.1

---
+++ DateTimePickers/app/src/main/java/com/example/reueljohn/datetimepickers/TimePickerFragment.java
@@ -0,0 +1,40 @@
+package com.example.reueljohn.datetimepickers;
+
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener
+{
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        final Calendar c = Calendar.getInstance();
+        int hour =c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+
+        activity.processTImePickerResult(hourOfDay, minute);
+
+    }
+}
