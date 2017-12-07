b289df004ac55481be62251021ae2fb11b52535e
Reuel John <reueljohn@MAC077.local>
2017-12-07 08:46:16 +0800

    9.2

---
+++ AppWithSettings/app/src/main/java/com/example/reueljohn/appwithsettings/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.reueljohn.appwithsettings;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static final String KEY_PREF_EXAMPLE_SWITCH = "example_switch";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
